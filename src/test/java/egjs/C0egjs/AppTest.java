f10386bde968406116ed10aff6ef40cb240e2a97
chand <chand@MSI>
2022-11-18 20:44:41 +0530

    eclipse->git->jenkins->sonarcube

---
+++ src/test/java/egjs/C0egjs/AppTest.java
@@ -0,0 +1,20 @@
+package egjs.C0egjs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
